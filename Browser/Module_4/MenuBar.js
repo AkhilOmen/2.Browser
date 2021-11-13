21e2ffdc9f2529430982ed283ad554ae4ce380de
AkhilOmen <akhilbv333@gmail.com>
2021-11-14 00:32:13 +0530

    Menu Bar CSS Completed

--- Browser/Module_4/MenuBar.js
+++ Browser/Module_4/MenuBar.js
@@ -50,5 +50,63 @@ fontstyleInput = document.addEventListener("click", function(e){
         }
         
     }
+    
+})
+
+// ...................Alignment Container....................
+let AlignmentInput = document.querySelector(".alignment_container");
+
+AlignmentInput.addEventListener("click", function(e){
+    let element = e.target;
+    // console.log(element);
+    let alignInput = element.classList[1].split("-")[2];
+    // console.log(alignInput);
+    if(alignInput == 'right' || alignInput == 'left' || alignInput == 'center' || alignInput == 'justify'){
+        let CellAddress = addressInput.value;
+        let CellObj = getridcid(CellAddress);
+        let Cell = document.querySelector(`.grid .cell[rid = '${CellObj.rid}'][cid = '${CellObj.cid}']`);
+        
+        Cell.style.textAlign = alignInput;
+        
+    }
 
 })
+
+// .................................... Color Container.......................................
+// ...................font color
+let textcolorInput = document.querySelector(".fa-tint");
+let hiddentextcolorInput = document.querySelector(".font_color_input");
+
+textcolorInput.addEventListener("click", function(){
+    hiddentextcolorInput.click();
+})
+
+hiddentextcolorInput.addEventListener("change", function(e){
+    let fontcolor = hiddentextcolorInput.value;
+    // console.log(fontcolor);
+    let CellAddress = addressInput.value;
+    let CellObj = getridcid(CellAddress);
+    let Cell = document.querySelector(`.grid .cell[rid = '${CellObj.rid}'][cid = '${CellObj.cid}']`);
+    
+    Cell.style.color = fontcolor;
+})
+
+//...............................BackGround color
+let backgroundInput = document.querySelector(".fa-fill-drip");
+let hiddenbackgroundInput = document.querySelector(".background_color_input");
+
+backgroundInput.addEventListener("click", function(){
+    hiddenbackgroundInput.click();
+})
+
+hiddenbackgroundInput.addEventListener("change", function(){
+    let backgroundclr = hiddenbackgroundInput.value;
+    console.log()
+    let CellAddress = addressInput.value;
+    let CellObj = getridcid(CellAddress);
+    let Cell = document.querySelector(`.grid .cell[rid = '${CellObj.rid}'][cid = '${CellObj.cid}']`);
+    
+    Cell.style.backgroundColor = backgroundclr;
+
+})
+
--- Browser/Module_4/script.js
+++ Browser/Module_4/script.js
@@ -82,5 +82,6 @@ function getridcid(address){
 }
 
 
-// 
+//........................DataBase............
+
 
